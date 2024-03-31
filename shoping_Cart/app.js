778ec910b59c3188b29e2c688e70572471386985
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-31 10:18:46 +0500

    created the cards using get the data from mongodb

--- shoping_Cart/app.js
+++ shoping_Cart/app.js
@@ -1,7 +1,11 @@
 const express=require('express')
 const app=express()
+const mongoose=requir('mongoose')
 const path=require('path')
 const bodyParser=require('body-parser')
+var cookieParser = require('cookie-parser')
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
 const formRoute=require('./routes/form')
 
 const game=require('./model/form')
@@ -13,6 +17,7 @@ require('./db/conn')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 //set the static path
@@ -20,6 +25,20 @@ app.use('static',express.static('static'))
 //for images upload file static
 app.use(express.static('upload'))
 
+
+//store the session
+app.use(session({
+    secret:'rmhamzaarif89',
+    resave:false,
+    saveUninitialized:false,
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    cookie:{maxAge:180 * 60 * 1000}
+}))
+app.use((req,res,next)=>{
+res.locals.session=req.session;
+next();
+
+})
 
 app.set('view engine','ejs')
 app.set('views','views')
@@ -29,7 +48,10 @@ app.set('views','views')
 app.use(formRoute)
 
 
-
+app.get('/add-to-cart/:id',(req,res)=>{
+    let id=req.params._id
+    
+})
 
 
 
